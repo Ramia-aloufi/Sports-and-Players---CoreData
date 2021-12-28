6a6fe27245e292bb37146aadd210939bf4534ef3
RAM111111 <r.r.99@windowslive.com>
2021-12-28 18:56:49 +0300

    Initial Commit

---
+++ Sports and Players/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sports and Players
+//
+//  Created by R on 24/05/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
